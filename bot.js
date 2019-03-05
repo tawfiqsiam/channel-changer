1da16bba6de2bc1238c23e90575f60bd65eb1b19
tawfiqsiam <toto.siam999@gmail.com>
2019-03-05 11:26:13 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+
+const Discord = require('discord.js');
+const tpoints = {};
+const vpoints = {};
+const jimp = require("jimp");
+const Canvas = require("canvas");
+const client = new Discord.Client();
+
+
+
+
+
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '526363779106209793').setName("W");
+client.channels.find('id', '526363779106209793').setName("We");
+client.channels.find('id', '526363779106209793').setName("Wel");
+client.channels.find('id', '526363779106209793').setName("Welc");
+client.channels.find('id', '526363779106209793').setName("Welco");
+client.channels.find('id', '526363779106209793').setName("Welcom");
+client.channels.find('id', '526363779106209793').setName("Welcome");
+client.channels.find('id', '526363779106209793').setName("Welcome T");
+client.channels.find('id', '526363779106209793').setName("Welcome To");
+client.channels.find('id', '526363779106209793').setName("Welcome To M");
+client.channels.find('id', '526363779106209793').setName("Welcome To Ma");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal S");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal Sh");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal Sho");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal Shop");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal Shop🔝");
+
+
+
+  }, 3000);
+});
+
+
+client.login(process.env.BOT_TOKEN);
